9c7e818b7dcbf5befdc3b02601d5a0965b3d264d
MiguellW <128272042+MiguellW@users.noreply.github.com>
2023-03-23 21:13:17 -0300

    Mudança teste

--- src/App.java
+++ src/App.java
@@ -3,8 +3,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
